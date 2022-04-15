c4e2a557cd01d73f578f6f7dcaabe819a10a38b5
mushui1995 <mushui1995@163.com>
2022-04-15 15:02:28 +0800

    pass

--- gnss_tool/test/a.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QApplication, QTabWidget, QPushButton
-
-app = QApplication()
-tabwidget = QTabWidget()
-tabwidget.addTab(QPushButton('tab1'), 'tab1')
-tabwidget.addTab(QPushButton('tab2'), 'tab2')
-tabwidget.show()
-app.exec()
---
+++ gnss_tool/test/test.py
@@ -0,0 +1,4 @@
+import time
+import datetime
+
+print("jenkins学习中！\n现在是%s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
